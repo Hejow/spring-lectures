faca68150bda1edb26c77d678896fb7362433629
Hejow <gmlwh124@naver.com>
2023-08-20 22:26:43 +0900

    230820: kotlin library

--- kotlin-to-java-library-app/src/main/java/com/group/libraryapp/service/user/UserService.java
+++ kotlin-to-java-library-app/src/main/java/com/group/libraryapp/service/user/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -28,7 +29,7 @@ public class UserService {
     public List<UserResponse> getUsers() {
         return userRepository.findAll().stream()
                 .map(UserResponse::new)
-                .toList();
+                .collect(Collectors.toList());
     }
 
     @Transactional
